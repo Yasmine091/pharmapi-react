365259ef0373a4c26d5d88ab1e543bdf6c3e15c2
Yasmine091 <yasmine.emdounan@gmail.com>
2021-03-24 09:44:36 +0100

    Structure : Install react + axios

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import './main.css'
+import axios from 'axios';
+
+class PharmAPI extends Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        }
+    }
+}
+
+export default PharmAPI;
